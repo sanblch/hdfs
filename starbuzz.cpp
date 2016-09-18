c2822641c067ea7fa6583769cdf53ad4d0760565
Alexander Krutikov <a.krutikov@innopolis.ru>
2016-09-18 07:47:19 +0000

    add setsize, getsize

--- starbuzz.cpp
+++ starbuzz.cpp
@@ -8,6 +8,12 @@
 
 class Beverage {
 public:
+  enum Size {
+    SMALL = -1,
+    MEDIUM = 0,
+    LARGE = 1
+  };
+  
   virtual ~Beverage() {}
   
   virtual std::string getDescription() {
@@ -16,11 +22,32 @@ public:
 
   virtual double cost() = 0;
 
+  virtual Size getSize() {
+    return size;
+  }
+    
+  virtual void setSize(Size size) {
+    this->size = size;
+  }
+
   std::string description { "Unknown description" };
+  Size size { MEDIUM };
 };
 
 class CondimentDecorator : public Beverage {
-  virtual std::string getDescription() override = 0;
+public:
+  CondimentDecorator(Beverage* beverage) : beverage(beverage) {};
+
+  Beverage::Size getSize() {
+    return beverage->getSize();
+  }
+
+  void setSize(Beverage::Size size) {
+    beverage->setSize(size);
+  }
+
+protected:
+  std::unique_ptr<Beverage> beverage;
 };
 
 class Espresso : public Beverage {
@@ -30,7 +57,7 @@ public:
   }
 
   double cost() {
-    return 1.99;
+    return 1.99 + .1 * getSize();
   }
 };
 
@@ -41,7 +68,7 @@ public:
   }
 
   double cost() {
-    return .89;
+    return .89 + .1 * getSize();
   }
 };
 
@@ -52,7 +79,7 @@ public:
   }
 
   double cost() {
-    return .99;
+    return .99 + .1 * getSize();
   }
 };
 
@@ -63,66 +90,60 @@ public:
   }
 
   double cost() {
-    return 1.05;
+    return 1.05 + .1 * getSize();
   }
 };
 
 class Mocha : public CondimentDecorator {
 public:
-  Mocha(Beverage* beverage) : beverage(beverage) {}
+  Mocha(Beverage* beverage) : CondimentDecorator(beverage) {}
 
   std::string getDescription() {
     return beverage->getDescription() + ", Mocha";
   }
 
   double cost() {
-    return .20 + beverage->cost();
+    return .20 + .05 * getSize() + beverage->cost();
   }
-
-private:
-  std::unique_ptr<Beverage> beverage;
 };
 
 class Soy : public CondimentDecorator {
 public:
-  Soy(Beverage* beverage) : beverage(beverage) {}
+  Soy(Beverage* beverage) : CondimentDecorator(beverage) {}
   
   std::string getDescription() {
     return beverage->getDescription() + ", Soy";
   }
 
   double cost() {
-    return .15 + beverage->cost();
+    return .15 + .05 * getSize() + beverage->cost();
   }
-
-private:
-  std::unique_ptr<Beverage> beverage;
 };
 
 class Whip : public CondimentDecorator {
 public:
-  Whip(Beverage* beverage) : beverage(beverage) {}
+  Whip(Beverage* beverage) : CondimentDecorator(beverage) {}
 
   std::string getDescription() {
     return beverage->getDescription() + ", Whip";
   }
 
   double cost() {
-    return .10 + beverage->cost();
+    return .10 + .05 * getSize() + beverage->cost();
   }
-
-private:
-  std::unique_ptr<Beverage> beverage;
 }; 
 
 int main() {
   std::unique_ptr<Beverage> beverage { std::make_unique<Espresso>() };
+  beverage->setSize(Beverage::MEDIUM);
   printf("%s $%f\n", beverage->getDescription().c_str(), beverage->cost());
 
   std::unique_ptr<Beverage> beverage2 { new Whip { new Mocha { new Mocha { new DarkRoast() }}}};
+  beverage2->setSize(Beverage::LARGE);
   printf("%s $%f\n", beverage2->getDescription().c_str(), beverage2->cost());
 
   std::unique_ptr<Beverage> beverage3 { new Whip { new Mocha { new Soy { new HouseBlend() }}}};
+  beverage3->setSize(Beverage::SMALL);
   printf("%s $%f\n", beverage3->getDescription().c_str(), beverage3->cost());
   return 0;
 }
